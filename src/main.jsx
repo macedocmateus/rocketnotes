0fbea7db1a71b5ae50099733fc3af705b630f603
Mateus Macedo <mateusmacedoeng@gmail.com>
2024-05-20 16:32:18 -0300

    add default into styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyles from './styles/global.js';
+
+import theme from './styles/theme.js';
+
 import { Details } from './pages/Details/index.jsx';
+
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Details />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Details />
+    </ThemeProvider>
   </React.StrictMode>,
 )
