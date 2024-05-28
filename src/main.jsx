c14faef743316666c5c7f5c1e2bdb8bbea8208f3
Mateus Macedo <mateusmacedoeng@gmail.com>
2024-05-28 06:29:26 -0300

    initial home

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import GlobalStyles from './styles/global.js';
 
 import theme from './styles/theme.js';
 
-import { Details } from './pages/Details/index.jsx';
+import { Home } from './pages/Home/index.jsx';
 
 
 
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Details />
+      <Home />
     </ThemeProvider>
   </React.StrictMode>,
 )
