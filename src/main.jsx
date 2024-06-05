49f1df4d11f416f05c8461ac48ca98dfdcffa75c
Mateus Macedo <mateusmacedoeng@gmail.com>
2024-06-05 14:02:37 -0300

    add page profile

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import GlobalStyles from './styles/global.js';
 
 import theme from './styles/theme.js';
 
-import { SignUp } from './pages/SignUp/index.jsx';
+import { Profile } from './pages/Profile/index.jsx';
 
 
 
@@ -15,7 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <SignUp />
+      <Profile />
     </ThemeProvider>
   </React.StrictMode>,
 )
