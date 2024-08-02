436c0c692ebd1c5051d1526efd7e10cd002d00e6
Mateus Macedo <mateusmacedoeng@gmail.com>
2024-08-02 17:52:34 -0300

    add usestate in profile page

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,13 +1,18 @@
-import { Container, Form, Avatar } from "./styles.js"
-import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from "react-icons/fi"
-import { Input } from "../../components/Input"
-import { Button } from "../../components/Button"
-import { Link } from "react-router-dom"
+import { useState } from 'react';
+import { Container, Form, Avatar } from './styles.js';
+import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from 'react-icons/fi';
+import { Input } from '../../components/Input';
+import { Button } from '../../components/Button';
+import { Link } from 'react-router-dom';
+import { useAuth } from '../../hooks/auth';
 
 export function Profile() {
-
+    const { user } = useAuth();
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [oldPassword, setOldPassword] = useState('');
+    const [newPassword, setNewPassword] = useState('');
     return (
-
         <Container>
             <header>
                 <Link to="/">
@@ -17,46 +22,25 @@ export function Profile() {
 
             <Form>
                 <Avatar>
-                    <img
-                        src="https://github.com/macedocmateus.png"
-                        alt="Foto do usuário"
-                    />
+                    <img src="https://github.com/macedocmateus.png" alt="Foto do usuário" />
 
                     <label htmlFor="avatar">
                         <FiCamera />
 
-                        <input
-                            id="avatar"
-                            type="file"
-                        />
+                        <input id="avatar" type="file" />
                     </label>
                 </Avatar>
 
-                <Input
-                    placeholder="Nome"
-                    type="text"
-                    icon={FiUser}
-                />
-                <Input
-                    placeholder="E-mail"
-                    type="text"
-                    icon={FiMail}
-                />
-                <Input
-                    placeholder="Senha atual"
-                    type="password"
-                    icon={FiLock}
-                />
-                <Input
-                    placeholder="Nova senha "
-                    type="password"
-                    icon={FiLock}
-                />
+                <Input placeholder="Nome" type="text" icon={FiUser} value={user.name} onChange={(e) => setName(e.target.value)} />
+
+                <Input placeholder="E-mail" type="text" icon={FiMail} value={user.email} onChange={(e) => setEmail(e.target.value)} />
+
+                <Input placeholder="Senha atual" type="password" icon={FiLock} onChange={(e) => setOldPassword(e.target.value)} />
+
+                <Input placeholder="Nova senha " type="password" icon={FiLock} onChange={(e) => setNewPassword(e.target.value)} />
 
                 <Button title="Salvar" />
             </Form>
         </Container>
-
     );
-
-};
+}
